c144f705ee0a840e9389b1f52bcbf35c7ec0c80d
tyleryoungblood <iphonetyler@gmail.com>
2014-07-26 16:31:55 -0400

    Define frontend route

--- scotch.io/todo/server.js
+++ scotch.io/todo/server.js
@@ -77,6 +77,12 @@ var Todo = mongoose.model('Todo', {
 	
 
 
+	// application -------------------------------------------------------------
+	app.get('*', function(req, res) {
+		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+	});
+
+
 // listen (start app with node server.js) ======================================
 app.listen(8080);
 console.log("App listening on port 8080");
